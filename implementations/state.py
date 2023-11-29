5eae49bdae9a94db2894a88b5fbab610cb77c0f8
A. Kim <kingalvinjkim@gmail.com>
2023-11-28 19:40:57 -0500

    I forgot to hit save before committing

--- implementations/state.py
+++ implementations/state.py
@@ -25,11 +25,10 @@ class State:
 
 
 def get_sensor_data_cost(sensor_data: SensorData) -> float:
+    # Sensor Range Limits and Optimal Ranges:
+    #  Temperature = [24,29) and [26,27]
+    #  Humidity = [70,80) and [75,80]
+    #  Light = 8am-10pm [850,950) / 10pm-8am LEDs off and daytime [860,940]
+    #  Soil Moisture = [500-650) and [550,600]
+    # TODO
     return 0
-
-
-# Sensor Range Limits and Optimal Ranges:
-#  Temperature = [24,29) and [26,27]
-#  Humidity = [70,80) and [75,80]
-#  Light = 8am-10pm [850,950) / 10pm-8am LEDs off and daytime [860,940]
-#  Soil Moisture = [500-650) and [550,600]
